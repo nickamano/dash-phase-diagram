7b8ee0df1c49d94efcad3a1c76067f2a80983da9
nickamano <nick.amano@yahoo.com>
2026-01-19 16:51:58 -0500

    feat: Update app layout, refine slider parameters, adjust temperature and mole fraction ranges, and add an informative footer.

--- app/app/main.py
+++ app/app/main.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc, Input, Output, State, callback
+from dash import Dash, html, dcc, Input, Output, State, callback, Patch
 import numpy as np
 import flask
 from app.app.convexhull import generate_data, visualize_convex_hull
@@ -6,97 +6,103 @@ from app.app.convexhull import generate_data, visualize_convex_hull
 # Phases of interest
 phases = ['A_solution','B_solution', 'AB_solution', 'liquid']
 # Temperature range from 300 - 2000 (inclusive with 1K increments)
-T_range = np.arange(1,2000,5)
+T_range = np.arange(300,2000,6)
 # Mole fraction step size
-x_grid = 0.01
+x_grid = 0.02
 # Mole fraction range from 0 to 1 (inclusive with x_grid increments)
 x = np.arange(0,1+x_grid,x_grid)
 
 # Used to silence arithmetic errors that arise due to log
 np.seterr(divide='ignore', invalid='ignore')
 
+footer = '''Made By [Nicholas Amano](https://websites.umich.edu/~namano/) at the University of Michigan.
+
+The above is a visualization demonstrating how phase diagrams are made. Using the regular solution model of the entropy of mixing, 
+we derive the Gibbs free energy curves of a solution A, a solution B, a liquid phase, and an AB solution phase in 
+composition and temperature space. We then observe if the convex hull of the curves lies on any given solution, if it does, then 
+that is the energetically stable phase, and if not, a two phase region is stable. Here, two phase regions is shaded __gray__,
+the pure single phases are __green__ and __red__, the liquid phase is __blue__, and the mixed solution is __yellow__. 
+'''
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = flask.Flask(__name__)
-dash_app = Dash(__name__, external_stylesheets=external_stylesheets, server = app, url_base_pathname = '/')
+dash_app = Dash(__name__, external_stylesheets=external_stylesheets, server = app, requests_pathname_prefix = '/rsm/')
+fig = visualize_convex_hull(generate_data(x, T_range), phases, x, T_range)
 
 dash_app.layout = html.Div([
-                html.Div([
+                html.Div([dcc.Markdown('''## 3D Gibbs Free Energy for Regular Solutions Model''', style={'text-align': 'center'}), 
                     html.Div([
-                        dcc.Markdown('''3D Gibbs Free Energy Graph using Regular Solutions Model '''),
-                        dcc.Graph(id='3D-Gibbs', config=dict(responsive=True)),
-                        html.Button('show phase diagram', id='camera-view', n_clicks=0, style={'margin':'2px'}),
+                        dcc.Graph(figure = fig, id='3D-Gibbs', config=dict(responsive=True)),
                         html.Button('reset parameters', id='reset', n_clicks=0, style={'margin':'2px'})], style={'width':'60%', 'float': 'left', 'text-align':'center'}),
                     html.Div([
-                        dcc.Markdown(id='w_AB-disp', style={'marginTop': 20,}, mathjax=True), 
+                        dcc.Markdown(id='w_AB-disp', style={'marginTop': 20}, mathjax=True),    
                         dcc.Slider(
-                                -100000, 100000, step=5,
-                                value=10000,
-                                marks={-100000 + 20000*i : '{}'.format(-100000 + 20000*i) for i in range(0,10)},
+                                -100, 100, step=.1,
+                                value=20,
+                                marks={-100 + 20*i : '{}'.format(-100 + 20*i) for i in range(0,11)},
                                 id='w-AB'),
-                        dcc.Markdown(id='L0-disp', style={'marginTop': 20,}, mathjax=True), 
+                        dcc.Markdown(id='L0-disp', style={'marginTop': 20}, mathjax=True), 
                         dcc.Slider(
-                                -100000, 100000, step=5,
-                                value=5000,
-                                marks={-100000 + 20000*i : '{}'.format(-100000 + 20000*i) for i in range(0,10)},
-                                id='L0'),
-                        dcc.Markdown(id='L1-disp', style={'marginTop': 20,}, mathjax=True), 
-                        dcc.Slider(
-                                -100000, 100000, step=5,
+                                -30, 20, step=0.1,
                                 value=0,
-                                marks={-100000 + 20000*i : '{}'.format(-100000 + 20000*i) for i in range(0,10)},
+                                marks={5* i - 30 : '{}'.format(5*i - 30) for i in range(0,11)},
+                                id='L0'),
+                        dcc.Markdown(id='L1-disp', style={'marginTop': 20}, mathjax=True), 
+                        dcc.Slider(
+                                -100, 100, step=.1,
+                                value=10,
+                                marks={-100 + 20*i : '{}'.format(-100 + 20*i) for i in range(0,11)},
                                 id='L1'),
-                        dcc.Markdown(id='HA-disp', style={'marginTop': 20,}, mathjax=True), 
+                        dcc.Markdown(id='HA-disp', style={'marginTop': 20}, mathjax=True), 
                         dcc.Slider(
-                                -100000, 100000, step=5,
-                                value=-35000,
-                                marks={-100000 + 20000*i : '{}'.format(-100000 + 20000*i) for i in range(0,10)},
+                                -100, 100, step=.1,
+                                value=-35,
+                                marks={-100 + 20*i : '{}'.format(-100 + 20*i) for i in range(0,11)},
                                 id='HA'),
-                        dcc.Markdown(id='HB-disp', style={'marginTop': 20,}, mathjax=True), 
+                        dcc.Markdown(id='HB-disp', style={'marginTop': 20}, mathjax=True), 
                         dcc.Slider(
-                                -100000, 100000, step=5,
-                                value=-38000,
-                                marks={-100000 + 20000*i : '{}'.format(-100000 + 20000*i) for i in range(0,10)},
+                                -100, 100, step=.1,
+                                value=-36,
+                                marks={-100 + 20*i : '{}'.format(-100 + 20*i) for i in range(0,11)},\
                                 id='HB'),
-                        dcc.Markdown(id='SA-disp', style={'marginTop': 20,}, mathjax=True), 
+                        dcc.Markdown(id='SA-disp', style={'marginTop': 20}, mathjax=True), 
                         dcc.Slider(
                                 0, 100, step=0.1,
                                 value=11.1,
-                                marks={i * 10 : '{}'.format(i*10) for i in range(0,10)},
+                                marks={i * 10 : '{}'.format(i*10) for i in range(0,11)},
                                 id='SA'),
-                        dcc.Markdown(id='SB-disp', style={'marginTop': 20,}, mathjax=True), 
+                        dcc.Markdown(id='SB-disp', style={'marginTop': 20}, mathjax=True), 
                         dcc.Slider(
                                 0, 100, step=0.1,
                                 value=2.4,
-                                marks={i * 10 : '{}'.format(i*10) for i in range(0,10)},
+                                marks={i * 10 : '{}'.format(i*10) for i in range(0,11)},
                                 id='SB'),
-                        dcc.Markdown(id='HAL-disp', style={'marginTop': 20,}, mathjax=True), 
+                        dcc.Markdown(id='HAL-disp', style={'marginTop': 20}, mathjax=True), 
                         dcc.Slider(
-                                -100000, 100000, step=5,
-                                value=-20000,
-                                marks={-100000 + 20000*i : '{}'.format(-100000 + 20000*i) for i in range(0,10)},
+                                -100, 100, step=.1,
+                                value=-20,
+                                marks={-100 + 20*i : '{}'.format(-100 + 20*i) for i in range(0,11)},
                                 id='HAL'),
-                        dcc.Markdown(id='HBL-disp', style={'marginTop': 20,}, mathjax=True), 
+                        dcc.Markdown(id='HBL-disp', style={'marginTop': 20}, mathjax=True), 
                         dcc.Slider(
-                                -100000, 100000, step=5,
-                                value=-25000,
-                                marks={-100000 + 20000*i : '{}'.format(-100000 + 20000*i) for i in range(0,10)},
+                                -100, 100, step=.1,
+                                value=-25,
+                                marks={-100 + 20*i : '{}'.format(-100 + 20*i) for i in range(0,11)},
                                 id='HBL'),
-                        dcc.Markdown(id='SAL-disp', style={'marginTop': 20,}, mathjax=True), 
+                        dcc.Markdown(id='SAL-disp', style={'marginTop': 20}, mathjax=True), 
                         dcc.Slider(
                                 0, 100, step=0.1,
                                 value=21.6,
-                                marks={i * 10 : '{}'.format(i*10) for i in range(0,10)},
+                                marks={i * 10 : '{}'.format(i*10) for i in range(0,11)},
                                 id='SAL'),
-                        dcc.Markdown(id='SBL-disp', style={'marginTop': 20,}, mathjax=True), 
+                        dcc.Markdown(id='SBL-disp', style={'marginTop': 20}, mathjax=True), 
                         dcc.Slider(
                                 0, 100, step=0.1,
                                 value=15,
-                                marks={i * 10 : '{}'.format(i*10) for i in range(0,10)},
+                                marks={i * 10 : '{}'.format(i*10) for i in range(0,11)},
                                 id='SBL'),
                         ], style={'width': '39%', "text-align": "left", 'float':'left', 'min-width':'480px'})
-                    ]),
-            html.Footer([dcc.Markdown('''By Nicholas Amano at the University of Michigan''')])])
+                    ]), html.Div([dcc.Markdown(footer)], style={'float':'left', 'clear': 'left'})])
 
 @callback(
     Output('3D-Gibbs', 'figure'),
@@ -112,88 +118,80 @@ dash_app.layout = html.Div([
     Input('HBL', 'value'),
     Input('SAL', 'value'),
     Input('SBL', 'value'))
-def update_omega_AB(layout, w_AB, L0, L1, HA, HB, SA, SB, HAL, HBL, SAL, SBL):
+def update_params(layout, w_AB, L0, L1, HA, HB, SA, SB, HAL, HBL, SAL, SBL):
     if layout != None and 'scene.camera' in layout.keys():
-        return visualize_convex_hull(generate_data(x, T_range, w_AB, L0, L1, HA, HB, SA, SB, HAL, HBL, SAL, SBL), phases, x, T_range, camera = layout['scene.camera'] )
+        return visualize_convex_hull(generate_data(x, T_range, w_AB * 1000, L0 * 1000, L1 * 1000, HA* 1000, HB* 1000, SA, SB, HAL* 1000, HBL* 1000, SAL, SBL), phases, x, T_range, camera = layout['scene.camera'] )
     camera = dict(up=dict(x=1, y=0, z=0),eye=dict(x=0, y=0, z=-2.5))
-    return visualize_convex_hull(generate_data(x, T_range, w_AB, L0, L1, HA, HB, SA, SB, HAL, HBL, SAL, SBL), phases, x, T_range, camera = camera)
+    patch_figure = Patch()
+    patch_figure['data'] = visualize_convex_hull(generate_data(x, T_range, w_AB * 1000, L0 * 1000, L1 * 1000, HA* 1000, HB* 1000, SA, SB, HAL* 1000, HBL* 1000, SAL, SBL), phases, x, T_range, camera = camera).data
+    return patch_figure
 
 @callback(
     Output('w_AB-disp', 'children'),
     Input('w-AB', 'value'))
 def display_value(value):
-    return fr'$w_{{ AB }}$ Value: {value}'
+    return fr'$w_{{ AB }}$: ${value} \frac{{kJ}}{{mol}}$'
 
 @callback(
     Output('L0-disp', 'children'),
     Input('L0', 'value'))
 def display_value(value):
-    return fr'$L_{{ 0 }}$ Value: {value}'
+    return fr'$L_{{ 0 }}$: ${value} \frac{{kJ}}{{mol}}$'
 
 @callback(
     Output('L1-disp', 'children'),
     Input('L1', 'value'))
 def display_value(value):
-    return fr'$L_{{ 1 }}$ Value: {value}'
+    return fr'$L_{{ 1 }}$: ${value} \frac{{kJ}}{{mol}}$'
 
 @callback(
     Output('HA-disp', 'children'),
     Input('HA', 'value'))
 def display_value(value):
-    return fr'$H_{{ A }}$ Value: {value}'
+    return fr'$H_{{ A }}$: ${value} \frac{{kJ}}{{mol}}$'
 
 @callback(
     Output('HB-disp', 'children'),
     Input('HB', 'value'))
 def display_value(value):
-    return fr'$H_{{ B }}$ Value: {value}'
+    return fr'$H_{{ B }}$: ${value} \frac{{kJ}}{{mol}}$'
 
 @callback(
     Output('SA-disp', 'children'),
     Input('SA', 'value'))
 def display_value(value):
-    return fr'$S_{{ A }}$ Value: {value}'
+    return fr'$S_{{ A }}$: ${value} \frac{{kJ}}{{mol \times K}}$'
 
 @callback(
     Output('SB-disp', 'children'),
     Input('SB', 'value'))
 def display_value(value):
-    return fr'$S_{{ B }}$ Value: {value}'
+    return fr'$S_{{ B }}$: ${value} \frac{{kJ}}{{mol \times K}}$'
 
 @callback(
     Output('HAL-disp', 'children'),
     Input('HAL', 'value'))
 def display_value(value):
-    return fr'$H_{{ AL }}$ Value: {value}'
+    return fr'$H_{{ AL }}$: ${value} \frac{{kJ}}{{mol}}$'
 
 @callback(
     Output('HBL-disp', 'children'),
     Input('HBL', 'value'))
 def display_value(value):
-    return fr'$H_{{ BL }}$ Value: {value}'
+    return fr'$H_{{ BL }}$: ${value} \frac{{kJ}}{{mol}}$'
 
 @callback(
     Output('SAL-disp', 'children'),
     Input('SAL', 'value'))
 def display_value(value):
-    return fr'$S_{{ AL }}$ Value: {value}'
+    return fr'$S_{{ AL }}$: ${value} \frac{{kJ}}{{mol}}$'
 
 @callback(
     Output('SBL-disp', 'children'),
     Input('SBL', 'value'))
 def display_value(value):
-    return fr'$S_{{ BL }}$ Value: {value}'
+    return fr'$S_{{ BL }}$: ${value} \frac{{kJ}}{{mol}}$'
 
-@callback(
-    Output('3D-Gibbs', 'figure', allow_duplicate=True),
-    Input('camera-view', 'n_clicks'),
-    State('3D-Gibbs', 'figure'),
-    prevent_initial_call=True)
-def reset_view(_, figure):
-    camera = dict(up=dict(x=1, y=0, z=0),eye=dict(x=0, y=0, z=-2.5))
-    if 'layout' in figure.keys() and 'scene' in figure['layout'].keys() and figure['layout']['scene']['camera'] != camera:
-        figure['layout']['scene']['camera'] = camera
-    return figure
 
 @callback(
     [Output('3D-Gibbs', 'figure', allow_duplicate=True),
@@ -210,8 +208,8 @@ def reset_view(_, figure):
     Output('SBL', 'value')],
     Input('reset', 'n_clicks'),
     prevent_initial_call=True)
-def reset_view(_):
-    return visualize_convex_hull(generate_data(x, T_range), phases, x, T_range), 10000, 5000, 0, -35000, -38000, 11.1, 2.4, -20000, -25000, 21.6, 15
+def reset_values(_):
+    return visualize_convex_hull(generate_data(x, T_range), phases, x, T_range), 10, 0, 10, -35000, -38000, 11.1, 2.4, -20000, -25000, 21.6, 15
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8050)
